13d05446978ac056beeb67a8a9d8d30c3568de78
taha <taha.azzabi@gmail.com>
2019-12-06 09:47:15 +0100

    first commit

---
+++ 2-using-shared-components/pages/about.js
@@ -0,0 +1,7 @@
+import Layout from "../comps/MyLayout";
+
+const contentPageAbout = <p>Hello Next.js</p>;
+
+const About = () => <Layout content={contentPageAbout} />;
+
+export default About;
---
+++ 2-using-shared-components/pages/index.js
@@ -0,0 +1,7 @@
+import Layout from "../comps/MyLayout";
+
+const contentPageIndex = <p>Hello Next.js</p>;
+
+const Index = () => <Layout content={contentPageIndex} />;
+
+export default Index;
